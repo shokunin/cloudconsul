53d3f2ca73dc504330cc61f924e15df96cd2de32
Christian Mague <cmague@konghq.com>
2018-01-25 09:30:24 -0800

    install and configure bind9

---
+++ manifests/bind.pp
@@ -0,0 +1,34 @@
+# install and configure bind9
+class cloudconsul::bind inherits cloudconsul {
+
+  package { 'bind9':
+    ensure => installed,
+  }
+
+  package { 'jinja2':
+    ensure   => installed,
+    provider => pip,
+  }
+
+  service { 'bind9':
+    ensure => stopped,
+    enable => false,
+  }
+
+  file { '/usr/local/bin/setup_dns_zone':
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => '0755',
+    source => 'puppet:///modules/cloudconsul/setup_dns_zone'
+  }
+
+  file { '/etc/supervsor.d/bind9.conf':
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => '0644',
+    source => 'puppet:///modules/cloudconsul/supervisord_bind'
+  }
+
+}
